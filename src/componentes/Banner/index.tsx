544d6f31023868a46432e44693aff5e21045f2ce
Diogo CS Berner <diogoberner@gmail.com>
2025-05-02 10:55:27 -0300

    feat: migrate components to TypeScript and update dependencies
    - Added TypeScript support by creating tsconfig.json.
    - Converted Banner and Botao components from JavaScript to TypeScript.
    - Updated App component to use the new Banner component with props.
    - Removed old JavaScript files for Banner and Botao components.
    - Added type definitions for Jest, Node, React, and React-DOM in package.json.
    - Updated package.json to include TypeScript as a dependency.

---
+++ src/componentes/Banner/index.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Banner.css";
+
+interface BannerProps {
+  src: string;
+  alt?: string;
+}
+
+const Banner = ({ src, alt }: BannerProps) => {
+  // JSX
+  return (
+    <header className="banner">
+      <img src={src} alt={alt} />
+    </header>
+  );
+};
+
+export default Banner;
